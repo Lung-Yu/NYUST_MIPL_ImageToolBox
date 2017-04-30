c1861ff332395d32b19c2270e0fba6bcaaea16cd
Lung-Yu <workfile975@gmail.com>
2017-04-30 13:59:08 +0800

    add Image Similarity
    添加影像相似度計算處理

---
+++ ImageSimilarity/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ImageSimilarity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile1 = new System.Windows.Forms.Button();
+            this.btnOpenFile2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnSimilarity = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile1
+            // 
+            this.btnOpenFile1.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile1.Name = "btnOpenFile1";
+            this.btnOpenFile1.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile1.TabIndex = 0;
+            this.btnOpenFile1.Text = "Open File";
+            this.btnOpenFile1.UseVisualStyleBackColor = true;
+            this.btnOpenFile1.Click += new System.EventHandler(this.btnOpenFile1_Click);
+            // 
+            // btnOpenFile2
+            // 
+            this.btnOpenFile2.Location = new System.Drawing.Point(249, 12);
+            this.btnOpenFile2.Name = "btnOpenFile2";
+            this.btnOpenFile2.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile2.TabIndex = 1;
+            this.btnOpenFile2.Text = "Open File";
+            this.btnOpenFile2.UseVisualStyleBackColor = true;
+            this.btnOpenFile2.Click += new System.EventHandler(this.btnOpenFile2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(251, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(233, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnSimilarity
+            // 
+            this.btnSimilarity.Location = new System.Drawing.Point(12, 258);
+            this.btnSimilarity.Name = "btnSimilarity";
+            this.btnSimilarity.Size = new System.Drawing.Size(75, 23);
+            this.btnSimilarity.TabIndex = 4;
+            this.btnSimilarity.Text = "Similarity";
+            this.btnSimilarity.UseVisualStyleBackColor = true;
+            this.btnSimilarity.Click += new System.EventHandler(this.btnSimilarity_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.label1.Location = new System.Drawing.Point(109, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSimilarity);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpenFile2);
+            this.Controls.Add(this.btnOpenFile1);
+            this.Name = "Form1";
+            this.Text = "Image Similarity Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile1;
+        private System.Windows.Forms.Button btnOpenFile2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnSimilarity;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageSimilarity/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageSimilarity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfDialog = new OpenFileDialog();
+            opfDialog.Filter = @"(*.bmp,*.jpg)|*.bmp;*.jpg;*.png";
+            opfDialog.FilterIndex = 3;
+            opfDialog.RestoreDirectory = true;
+            opfDialog.InitialDirectory = @"C:\\";
+
+
+            if (opfDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = opfDialog.FileName;
+            }
+        }
+
+        private void btnOpenFile2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfDialog = new OpenFileDialog();
+            opfDialog.Filter = @"(*.bmp,*.jpg)|*.bmp;*.jpg;*.png";
+            opfDialog.FilterIndex = 3;
+            opfDialog.RestoreDirectory = true;
+            opfDialog.InitialDirectory = @"C:\\";
+
+
+            if (opfDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.ImageLocation = opfDialog.FileName;
+            }
+        }
+
+        public static int[] GetGrayImagePixels(Bitmap img)
+        {
+            BitmapData data = img.LockBits(new System.Drawing.Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int[] histogram = new int[256];
+            unsafe
+            {
+                byte* ptr = (byte*)data.Scan0;
+                int remain = data.Stride - data.Width * 3;
+                for (int i = 0; i < histogram.Length; i++)
+                    histogram[i] = 0;
+                for (int i = 0; i < data.Height; i++)
+                {
+                    for (int j = 0; j < data.Width; j++)
+                    {
+                        int mean = ptr[0] + ptr[1] + ptr[2];
+                        mean /= 3;
+                        histogram[mean]++;
+                        ptr += 3;
+                    }
+                    ptr += remain;
+                }
+            }
+            img.UnlockBits(data);
+            return histogram;
+        }
+
+        private static float GetAbs(int firstNum, int secondNum)
+        {
+            float abs = Math.Abs((float)firstNum - (float)secondNum);
+            float result = Math.Max(firstNum, secondNum);
+            if (result == 0)
+                result = 1;
+            return abs / result;
+        }
+
+        //最終計算結果
+        public static float GetResult(int[] firstNum, int[] scondNum)
+        {
+            if (firstNum.Length != scondNum.Length)
+                return 0;
+
+            float result = 0;
+            int length = Math.Max(firstNum.Length, scondNum.Length);
+            for (int i = 0; i < length; i++)
+                result += 1 - GetAbs(firstNum[i], scondNum[i]);
+            return result / length;
+        }
+
+        private void btnSimilarity_Click(object sender, EventArgs e)
+        {
+            int[] image1 = GetGrayImagePixels(new Bitmap(pictureBox1.Image, 400, 400));
+            int[] image2 = GetGrayImagePixels(new Bitmap(pictureBox2.Image, 400, 400));
+
+            float result = GetResult(image1, image2);
+
+            label1.Text = "similarity : " + result;
+        }
+    }
+}
