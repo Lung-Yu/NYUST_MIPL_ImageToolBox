7c889eaad12a4bed9ac14b5e390dc04fea9ef377
Lung-Yu <workfile975@gmail.com>
2017-06-10 02:31:05 +0800

    UI Update

--- ImageProcessToolBox/FormShowImage.Designer.cs
+++ ImageProcessToolBox/FormShowImage.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1024, 1024);
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 1007);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormShowImage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1053, 1045);
+            this.ClientSize = new System.Drawing.Size(1058, 1045);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "FormShowImage";
             this.Text = "FormShowImage";
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ImageProcessToolBox/FormShowImage.cs
+++ ImageProcessToolBox/FormShowImage.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +14,53 @@ namespace ImageProcessToolBox
 {
     public partial class FormShowImage : Form
     {
-        
+
         public FormShowImage(Bitmap img)
         {
             InitializeComponent();
 
             pictureBox1.Image = img;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = @"(*.bmp,*.jpg)|*.bmp;*.jpg;*.png";
+
+            sfd.FilterIndex = 3;
+            sfd.RestoreDirectory = true;
+            if (DialogResult.OK == sfd.ShowDialog())
+            {
+                ImageFormat format = ImageFormat.Jpeg;
+                switch (Path.GetExtension(sfd.FileName).ToLower())
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                    default:
+                        MessageBox.Show(this, "Unsupported image format was specified", "Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+                try
+                {
+                    bitmap.Save(sfd.FileName, format);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(this, "Failed writing image file", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
