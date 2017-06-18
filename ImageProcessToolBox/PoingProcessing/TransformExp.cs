78fd60be8df81e6da2e88de78566cb7c0d231a1e
Lung-Yu <workfile975@gmail.com>
2017-06-18 09:17:55 +0800

    new transform exp

---
+++ ImageProcessToolBox/PoingProcessing/TransformExp.cs
@@ -0,0 +1,39 @@
+﻿using ImageProcessToolBox.BasicModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessToolBox.PoingProcessing
+{
+    class TransformExp : ImageBasic
+    {
+        private byte[] _values = new byte[256];
+        private void initValues()
+        {
+            for (int i = 0; i < _values.Length; i++)
+            {
+                double val = Math.Exp(i + 1);
+                _values[i] = (byte)((val > 255) ? 255 : val);
+            }
+        }
+
+        public override void process()
+        {
+            initValues();
+            for (int y = 0; y < _height; y++)
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    int r = _imgMap[x, y, IMAGE_RED_INDEX];
+                    int g = _imgMap[x, y, IMAGE_Green_INDEX];
+                    int b = _imgMap[x, y, IMAGE_Blue_INDEX];
+                    _resultMap[x, y, IMAGE_RED_INDEX] = _values[r];
+                    _resultMap[x, y, IMAGE_Green_INDEX] = _values[g];
+                    _resultMap[x, y, IMAGE_Blue_INDEX] = _values[b];
+                }
+            }
+        }
+    }
+}
