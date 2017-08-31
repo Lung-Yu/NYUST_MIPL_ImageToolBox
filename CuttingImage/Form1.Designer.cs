ba81721fb16bae0916f6bc1d25c6072d7a552350
MIPL <mipl.yuntech@gamil.com>
2017-08-31 14:26:22 +0800

    update

---
+++ CuttingImage/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CuttingImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_X = new System.Windows.Forms.Label();
+            this.label_Y = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1298, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開啟圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1298, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "儲存圖片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_X
+            // 
+            this.label_X.AutoSize = true;
+            this.label_X.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label_X.Location = new System.Drawing.Point(1298, 82);
+            this.label_X.Name = "label_X";
+            this.label_X.Size = new System.Drawing.Size(19, 16);
+            this.label_X.TabIndex = 4;
+            this.label_X.Text = "X";
+            // 
+            // label_Y
+            // 
+            this.label_Y.AutoSize = true;
+            this.label_Y.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label_Y.Location = new System.Drawing.Point(1298, 108);
+            this.label_Y.Name = "label_Y";
+            this.label_Y.Size = new System.Drawing.Size(19, 16);
+            this.label_Y.TabIndex = 5;
+            this.label_Y.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(1324, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(1324, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(1298, 148);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 83);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 761);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Y);
+            this.Controls.Add(this.label_X);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_X;
+        private System.Windows.Forms.Label label_Y;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ CuttingImage/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuttingImage
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _img;
+        private byte[, ,] _imgMap;
+        private double _width;
+        private double _height;
+
+        private static readonly int pixelSizeWidth = 512;
+        private static readonly int pixelSizeHeight = 512;
+        int SIZE_WIDTH = 100;
+        int SIZE_HEIGHT = 200;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+
+
+            Bitmap bitmap = new Bitmap(pictureBox2.Image);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = @"(*.bmp,*.jpg)|*.bmp;*.jpg;*.png";
+
+            sfd.FilterIndex = 3;
+            sfd.RestoreDirectory = true;
+            if (DialogResult.OK == sfd.ShowDialog())
+            {
+                ImageFormat format = ImageFormat.Jpeg;
+                switch (Path.GetExtension(sfd.FileName).ToLower())
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                    default:
+                        MessageBox.Show(this, "Unsupported image format was specified", "Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+                try
+                {
+                    bitmap.Save(sfd.FileName, format);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(this, "Failed writing image file", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfDialog = new OpenFileDialog();
+            opfDialog.Filter = @"(*.bmp,*.jpg,*.tif)|*.bmp;*.jpg;*.png;*.tif";
+            opfDialog.FilterIndex = 3;
+            opfDialog.RestoreDirectory = true;
+            opfDialog.InitialDirectory = @"C:\\";
+
+            if (opfDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = opfDialog.FileName;
+                //_img = new Bitmap(opfDialog.FileName);
+                initBitmap(opfDialog.FileName);
+            }
+
+        }
+
+
+        private void initBitmap(string fileAddr)
+        {
+            _img = new Bitmap(fileAddr);
+            _imgMap = pre(_img);
+
+            _width = _img.Width;
+            _height = _img.Height;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_img == null)
+            {
+                MessageBox.Show("無圖片");
+                return;
+            }
+
+            //顯示滑鼠座標
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+
+            relocate(e.X, e.Y);
+        }
+
+
+
+        private void relocate(int x, int y)
+        {
+            //bool isWidth = false;
+            //if (_width > _height)
+            //    isWidth = true;
+
+            //if (isWidth)
+            //{
+            //double wUnit = (double)pixelSizeWidth / _width;
+            //double hUnit = (double)pixelSizeHeight / _height;
+
+
+            //double offset = (_height / 2) * hUnit;
+            //double footerSide = pixelSizeHeight - offset;
+
+            //if (y < offset || y > footerSide)
+            //{
+            //    MessageBox.Show("超出範圍");
+            //    return;
+            //}
+            //else
+            //{
+            try
+            {
+                byte[, ,] map = new byte[SIZE_WIDTH, SIZE_HEIGHT, 3];
+
+                for (int ix = 0; ix < SIZE_WIDTH; ix++)
+                {
+                    for (int iy = 0; iy < SIZE_HEIGHT; iy++)
+                    {
+                        byte t = _imgMap[ix, iy, 0];
+                        int ny = iy + y - SIZE_HEIGHT / 2;
+                        int nx = ix + x - SIZE_WIDTH / 2;
+                        map[ix, iy, 0] = _imgMap[nx, ny, 0];
+                        map[ix, iy, 1] = _imgMap[nx, ny, 1];
+                        map[ix, iy, 2] = _imgMap[nx, ny, 2];
+                    }
+                }
+
+
+                Bitmap dst = new Bitmap(SIZE_WIDTH, SIZE_HEIGHT);
+                writeBitmap(dst, map);
+
+                pictureBox2.Image = dst;
+            }
+            catch (Exception)
+            {
+
+            }
+            
+            //}
+
+            //}
+            //else
+            //{
+
+            //}
+        }
+
+        private static byte[, ,] pre(Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            byte[, ,] resMatrix = new byte[width, height, 4];  //回傳矩陣 
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+
+            //將srcBitmap鎖定到系統內的記憶體的某個區塊中，並將這個結果交給BitmapData類別的srcBimap
+            BitmapData srcBmData = bitmap.LockBits(rect, ImageLockMode.ReadWrite,
+            PixelFormat.Format24bppRgb);
+
+            //位元圖中第一個像素數據的地址。它也可以看成是位圖中的第一個掃描行
+            //目的是設兩個起始旗標srcPtr、dstPtr，為srcBmData、dstBmData的掃描行的開始位置
+            System.IntPtr srcScan = srcBmData.Scan0;
+
+            unsafe //啟動不安全代碼
+            {
+                byte* srcP = (byte*)srcScan;
+                int srcOffset = srcBmData.Stride - width * 3;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++, srcP += 3)
+                    {
+                        resMatrix[x, y, 0] = *(srcP + 2);
+                        resMatrix[x, y, 1] = *(srcP + 1);
+                        resMatrix[x, y, 2] = *(srcP);
+                    }
+                    srcP += srcOffset;
+                }
+            }
+
+            bitmap.UnlockBits(srcBmData);
+
+            return resMatrix;
+        }
+
+        private static void writeBitmap(Bitmap bitmap, byte[, ,] martix)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData srcBmData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            System.IntPtr srcScan = srcBmData.Scan0;
+
+            unsafe
+            {
+                byte* srcP = (byte*)srcScan;
+                int srcOffset = srcBmData.Stride - width * 3;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++, srcP += 3)
+                    {
+                        //要修改
+                        *(srcP + 2) = martix[x, y, 0];
+                        *(srcP + 1) = martix[x, y, 1];
+                        *(srcP) = martix[x, y, 2];
+                    }
+                    srcP += srcOffset;
+                }
+            }
+            bitmap.UnlockBits(srcBmData);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
